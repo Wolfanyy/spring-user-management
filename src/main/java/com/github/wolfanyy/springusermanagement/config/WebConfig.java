e4d774ed0a2aba7eb9887bc1d673658918421366
Wolfanyy <gribanova.anna98@mail.ru>
2026-06-25 16:31:19 +0400

    feat: add user listing functionality

--- src/main/java/com/github/wolfanyy/springusermanagement/config/WebConfig.java
+++ src/main/java/com/github/wolfanyy/springusermanagement/config/WebConfig.java
@@ -59,6 +59,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
         registry.addResourceHandler("/css/**")
                 .addResourceLocations("/resources/css/");
 
--- src/main/java/com/github/wolfanyy/springusermanagement/controller/UserController.java
+++ src/main/java/com/github/wolfanyy/springusermanagement/controller/UserController.java
@@ -1,14 +1,28 @@
 package com.github.wolfanyy.springusermanagement.controller;
 
+import com.github.wolfanyy.springusermanagement.service.UserService;
+import com.github.wolfanyy.springusermanagement.service.UserServiceImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
 
-    @GetMapping("/users")
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
     public String findAll(Model model) {
+        model.addAttribute
+                ("users",
+                        userService.findAll()
+                );
         return "users/list";
     }
 }
